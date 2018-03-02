460646eff5089497dd2a03533a166eff97d38699
Vincent <work@vincenttran.me>
2018-03-02 23:26:51 +1100

    Modified default airdrop amount to 1 token per account. Modified initial token starting balance to match that of bitfwd tutorial 1.

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const provider = new providers.JsonRpcProvider('http://localhost:8545', 'ropsten
 const contractAddress = config.contractAddress;
 const abi = config.abi;
 
-const AIRDROP_QTY = "1000000000000000000000";
+const AIRDROP_QTY = "1000000000000000000";
 const DURATION = 0.1;
 const GAS_LIMIT = 300000;
 const GAS_PRICE = 5000000000; // 5 gwei
 const STARTING_INDEX = -1;
-const INITIAL_TOKENS = "1000000000000000000000000";
+const INITIAL_TOKENS = "100000000000000000000000000";
 
 let wallet;
 if (config.seed)
